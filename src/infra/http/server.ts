2c9ce7a2ee70730cd9e417497a7c202a1ad96150
abacaxi <deco.neto10@gmail.com>
2025-12-01 23:01:53 -0300

    project setup

---
+++ src/infra/http/server.ts
@@ -0,0 +1,12 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+await server.register(fastifyCors, {
+  origin: '*',
+})
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running on http://localhost:3333')
+})
